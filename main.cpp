7573410068aa58d17ee2654279772ee140d7dc19
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-20 21:16:17 -0600

    Included cout statements for debugging purposes

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main(int argc,char** argv) {
     ifstream inFS(argv[1]);
 
     if (inFS.is_open()) {
+        cout << "Reading input file..." << endl;
         string str;
         getline(inFS,str,'\n');
 
+        cout << "Writing to output file..." << endl;
         ofstream inOF("../data/output.txt");
         inOF << "This is another string" << endl;
     } else {
