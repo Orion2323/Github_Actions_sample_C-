d9626347d00d7cca668d441b873951709ba44f7d
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-20 20:33:45 -0600

    Created input and output txt file to trigger Github Actions workflow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    ifstream inFS("../input.txt");
+
+    if (inFS.good()) {
+        string str;
+        getline(inFS,str,'\n');
+
+        ofstream inOF("../output.txt");
+        inOF << "This is another string" << endl;
+    } else {
+        cout << "File not open!" << endl;
+    }
     return 0;
 }
