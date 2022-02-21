59348b380bb02d8715e31bbe16bb93b0dd906a0f
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-20 21:27:35 -0600

    FIX: output stream accepts command line parameters instead of hardcoded file path

--- main.cpp
+++ main.cpp
@@ -6,14 +6,22 @@ using namespace std;
 int main(int argc,char** argv) {
     ifstream inFS(argv[1]);
 
+    // check if input file is open
     if (inFS.is_open()) {
         cout << "Reading input file..." << endl;
         string str;
         getline(inFS,str,'\n');
 
         cout << "Writing to output file..." << endl;
-        ofstream inOF("../data/output.txt");
-        inOF << "This is another string" << endl;
+        ofstream inOF(argv[2]);
+
+        // check if output file is open
+        if (inOF.is_open()) {
+            inOF << "This is another string" << endl;
+        } else {
+            cout << "Output file not created!" << endl;
+        }
+
     } else {
         cout << "File not open!" << endl;
     }
